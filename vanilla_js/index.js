c74bb333df0ca5530deb8fa336317e6df7685438
hitzza <cslzh4433@gmail.com>
2022-08-24 20:18:38 +0900

    ex index.html css end

---
+++ vanilla_js/index.js
@@ -0,0 +1,16 @@
+/*
+const arr = [1,2,3]
+arr.forEach(console.log);
+arr.forEach(alert);
+console.log(score);
+scroe =80;
+var score;
+console.log(score)
+var a = 0.1 + 1.1
+i = parseInt(a)
+f = parseFloat(a)
+console.log(i===f)
+*/
+
+console.log('test');
+//control + option + n  == run javascriptß
