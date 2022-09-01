d726d7c3ccfc3c000d0ee3fe49eef0a49ee7ae25
hitzza <cslzh4433@gmail.com>
2022-09-01 17:36:45 +0900

    js arr test

--- vanilla_js/index.js
+++ vanilla_js/index.js
@@ -11,11 +11,24 @@ i = parseInt(a)
 f = parseFloat(a)
 console.log(i===f)
 */
-
+/*
 let tyepTest1 = undefined;
 let tyepTest2 = null;
 console.log(typeof tyepTest1,typeof tyepTest2);
 console.log(tyepTest1, tyepTest2)
 console.log(tyepTest1 == tyepTest2);
 console.log(tyepTest1 === tyepTest2);
-//control + option + n  == run javascriptß
+*/
+const arrtest = [];
+arrtest[2] = 3;
+console.log(arrtest, arrtest.length);
+const und=[];
+let undlet;
+console.log(und);
+console.log(undlet);
+console.log(Array.isArray(und));
+console.log(Array.isArray(arrtest));
+console.log(Array.isArray(undlet));
+
+
+//control + option + n  == run javascript
