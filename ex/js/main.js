049b6c9ea56fa868ef91ebc20c2b97864f908ba7
hitzza <cslzh4433@gmail.com>
2022-09-07 21:14:36 +0900

    slide action1

--- ex/js/main.js
+++ ex/js/main.js
@@ -26,11 +26,28 @@ backToTop.addEventListener('click', moveBackToTop);
 const slidePrevList = document.getElementsByClassName('slide-prev');//모든 prev버튼 선택해서 배열 형태로 저장
 function transformPrev(event){//even에 어느 객체에서 어떤 이벤트를 받았는지 저장
     const slidePrev = event.target;//왼쪽 화살표 아이콘
-    const slidenext = slidePrev.nextElementSibling;//오른쪽 화살표 아이콘
+    const slideNext = slidePrev.nextElementSibling;//오른쪽 화살표 아이콘
 
     const classList = slidePrev.parentElement.parentElement.nextElementSibling;//ul태그 선택
     let activeLi = classList.getAttribute('data-position');
     const liList = classList.getElementsByTagName('li');
+
+    if(classList.clientWidth < (liList.length *260 + Number(activeLi))){//ul 전체 길이가 카드 값 보다 작으면(슬라이드가 넘쳐 있다면)
+        activeLi = Number(activeLi)- 260;//li요소를 왼쪽으로 260만큼 이동
+
+        if(classList.clientWidth > (liList.length *260 + Number(activeLi))){//ul 전체 길이가 카드 값 보다 크면(슬라이드가 끝까지 넘어 갔다면)
+            
+            slideNext.style.color = '#cfd8dc';//오른쪽 버튼 비활성화
+            slideNext.classList.remove('slide-prev-hover');
+        }
+
+        slideNext.style.color = '#2f3059';//오른쪽 버튼 활성화
+        slideNext.classList.add('slide-next-hover');
+
+    }
+    classList.style.transition = 'transform 1s';
+    classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+    classList.setAttribute('data-position', activeLi)
 }
 
 for (let i =0; i< slidePrevList.length;i++){//슬라이드 왼쪽 화살표 버튼 아이콘 모두 선택
