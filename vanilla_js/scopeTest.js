ba652f97283065aadc0a4740def9f34756abfae7
hitzza <cslzh4433@gmail.com>
2023-03-27 18:54:15 +0900

    scopeTest

---
+++ vanilla_js/scopeTest.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-fallthrough */
+/* eslint-disable no-case-declarations */
+let item = 1;
+
+switch (item) {
+  case 1:
+    let sport;
+    break;
+  case 2:
+  // let sport;
+  default:
+    console.log(sport);
+}
+//case, default는 블록 스코프가 아니기 때문에 에러 x
