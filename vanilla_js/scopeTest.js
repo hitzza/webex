be32cfb199bfd66c503ad346cf3a0903da18c7e4
hitzza <cslzh4433@gmail.com>
2023-03-28 14:51:25 +0900

    scopeTest

--- vanilla_js/scopeTest.js
+++ vanilla_js/scopeTest.js
@@ -11,4 +11,13 @@ switch (item) {
   default:
     console.log(sport);
 }
-//case, default는 블록 스코프가 아니기 때문에 에러 x
+//case, default는 블록 스코프가 아님
+
+let sport = "축구";
+
+if (sport) {
+  //   let sport = "농구";
+  console.log("안:", sport);
+  //블록 스코프 안쪽 변수가 블록 스코프 밖의 변수를 참조 할 수 있지만 바깥쪽 변수가 안쪽 변수로는 불가능
+}
+console.log("밖:", sport);
